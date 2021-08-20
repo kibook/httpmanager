3f8a57941b1c4856ca7f7a3a64f5189e5cf4a19a
kibook <kibuzhj@gmail.com>
2021-08-19 23:45:25 -0400

    Update routes example

--- examples/routes/server.lua
+++ examples/routes/server.lua
@@ -1,6 +1,6 @@
 SetHttpHandler(exports.httpmanager:createHttpHandler {
 	routes = {
-		["/players/(.+)"] = function(req, res, helpers, player)
+		["/players/(%d+)"] = function(req, res, helpers, player)
 			if GetPlayerEndpoint(player) then
 				res.send(GetPlayerName(player))
 			else
