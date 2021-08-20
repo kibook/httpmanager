a6353bdbb8a9a4763192d3d1976495353cfb0c05
kibook <kibuzhj@gmail.com>
2021-08-20 02:37:42 -0400

    Update auth example

--- examples/auth/server.lua
+++ examples/auth/server.lua
@@ -1,6 +1,11 @@
 SetHttpHandler(exports.httpmanager:createHttpHandler {
-	auth = {
+	authorization = {
 		["admin"] = "$2a$11$AK6NWflKbeYmnEKfEkUC5uxZ.OPuoJoBPR8yQy5HBIqXxvOpMJwli", -- "password"
 		["user"] = "$2a$11$pFxR7jD3/m4XYfkQy9u5LeQ8t0AaSRDNqgV1ih/Zwtq1KY2SMB2kK", -- "password"
+	},
+	access = {
+		{path = "/admin/.*", login = {["admin"] = true}},
+		{path = "/public/.*", login = false},
+		{path = "/public/secret/.*"}
 	}
 })
